2845223fcb580e90794cf216edbd0866e45ff6a7
DYernar <eduisebai.1@gmail.com>
2020-08-10 11:48:01 +0600

    fd

--- home_page.go
+++ home_page.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
+	print(freeRooms)
 	json, _ := json.Marshal(freeRooms)
+	fmt.Println(json)
 	w.Write(json)
 }
